1328912285b4923356efd3579770ef2ce80650dc
cshuo <cshuo@cshuo.com>
2015-04-15 21:05:55 +0800

    modify some unit

--- ganglia/static/ganglia/dist/js/index_data.js
+++ ganglia/static/ganglia/dist/js/index_data.js
@@ -1,6 +1,8 @@
 var donut;   // for draw host status graph
 var down_num = 0, up_num = 0;    //for update donut graph
+var metric_plot;   //global plot obj for plotting 
 
+//init module hosts status
 $(function() {                    	
 	var num_pair = get_host_num();
 	up_num = num_pair[0];            
@@ -20,11 +22,138 @@ $(function() {
 
 //init cluster resource utilization
 $(function(){
-    update_util();
+    update_util();    
 });
 
 //update the Host up/down status every 120 seconds 
 setInterval(update_host_status,120000);
+
+//init cluster overview to cpu usg
+$(function(){    
+    var init_dataset = data_for_plot("cpu");   // init to show cluster cpu util 
+    metric_plot = $.plot("#area-chart", init_dataset, {
+        xaxis: {
+                    mode: "time",                    
+                    timeformat: "%H:%M:%S"
+                },
+        grid: {
+                borderColor: "#f3f3f3",
+                tickColor: "#f3f3f3",
+                borderWidth: 1,
+                hoverable: true,          
+                clickable: true           
+              },
+        series: {
+            stack: false,
+            lines: {
+                show: true,
+                fill: true
+            },
+            points: 
+            {
+                show: false
+            }            
+        },        
+    	legend: {
+    	    noColumns: 4,
+    	    position: 'nw'
+    	}
+    });        
+
+    var op = {
+            hour: "2-digit", minute: "2-digit", second: "2-digit"
+        };    
+
+    $("<div id='tooltip'></div>").css({
+            position: "absolute",
+            display: "none",
+            border: "1px solid #66ffee",
+            padding: "2px",
+            "background-color": "#ffeeff", 
+            "border-radius":"3px",
+            opacity: 0.9
+        }).appendTo("body");
+
+    $("#area-chart").bind("plothover", function (event, pos, item) {
+            if (item) {                
+                var x = item.datapoint[0]/1000,
+                y = item.datapoint[1].toFixed(2);
+                var t = new Date(1970,0,1);                              
+                t.setSeconds(x);
+                $("#tooltip").html(t.toLocaleDateString("en-US",op)+"<br/>"+item.series.label+": "+y).  
+                css({top: item.pageY+5, left: item.pageX+5}).
+                fadeIn(150);
+            } else {
+                $("#tooltip").hide();
+            }
+    });
+});
+
+/*function for getting usg data for specific host's metric*/
+function get_hour_metric(mtc_name){    
+    var v_array = [];
+    var base_url = "http://114.212.189.132:8000/ganglia/hour_mtc_avg/" + mtc_name;
+    var xml_doc;
+    var xml_http = new XMLHttpRequest();    
+    
+    xml_http.open("GET",base_url,false);
+    xml_http.send();
+    xml_doc = jQuery.parseXML(xml_http.responseText); 
+
+    $(xml_doc).find('row').each(function(){
+            var time = parseFloat($(this).children(":eq(0)").text());
+            var sum = parseFloat($(this).children(":eq(1)").text());
+            var num = parseFloat($(this).children(":eq(2)").text())
+            var avg = (sum/num).toFixed(2);
+            v_array.push([time*1000,avg]);      //time series xaxis require x to be millis
+            });            
+    return v_array;
+};
+
+
+/*get all metric data of res from all hosts and preparing for plotting*/
+function data_for_plot(res_name){
+    var mtc_1,mtc_2,mtc_3,mtc_4;
+    var d1,d2,d3,d4;
+    var datasets;    
+    if(res_name == "cpu"){        
+        d1 = get_hour_metric("cpu_user");
+        d2 = get_hour_metric("cpu_system");
+        d3 = get_hour_metric("cpu_nice");
+        d4 = get_hour_metric("cpu_idle");        
+        datasets = [{data:d1,label:"cpu_user  ",color:"#3c8d00"},{data:d2,label:"cpu_system",color:"#3c8dcc"},
+        {data:d3,label:"cpu_wait",color:"#eeeeff"},{data:d4,label:"cpu_idle",color:"#66ffee"}];               
+    } 
+    if(res_name == "mem"){        
+        d1 = get_hour_metric("mem_free");
+        d2 = get_hour_metric("mem_cached");
+        d3 = get_hour_metric("mem_buffers");
+        d4 = get_hour_metric("mem_total");
+        datasets = [{data:d1,label:"mem_free",color:"#3c8d00"},{data:d2,label:"mem_cached",color:"#3c8dcc"},
+        {data:d3,label:"mem_buffers",color:"#eeeeff"},{data:d4,label:"mem_total",color:"#66ffee"}];               
+    }
+    if(res_name == "load"){        
+        d1 = get_hour_metric("load_one");
+        d2 = get_hour_metric("load_five");
+        d3 = get_hour_metric("load_fifteen");        
+        datasets = [{data:d1,label:"load_one",color:"#3c8d00"},{data:d2,label:"load_five",color:"#3c8dcc"},
+        {data:d3,label:"load_fifteen",color:"#3c8dff"}];        
+    }
+    if(res_name == "network"){        
+        d1 = get_hour_metric("bytes_in");
+        d2 = get_hour_metric("bytes_out");
+        datasets = [{data:d1,label:"bytes_in",color:"#3c8d00"},{data:d2,label:"bytes_out",color:"#3c8dcc"}];        
+    }
+    return datasets;
+}
+
+//for drawdown list click actions
+function show_metric(res_name){
+    var datasets = data_for_plot(res_name);
+    metric_plot.setData(datasets);
+    metric_plot.setupGrid();
+    metric_plot.draw();
+}
 
 //function for updating the cluster resourses untilization 
 function update_util(){
@@ -57,27 +186,6 @@ function get_mtc_avg(mtc_name){
 	num = parseInt(v_arr[1]);			
 	return sum/num;
 }
-
-
-/*get avg of the requested resource value in last 150s*/
-// function get_util_avg(req_url){
-//     var count=0, sum = 0;    
-//     var xml_http = new XMLHttpRequest();
-//     xml_http.open("GET",req_url,false);
-//     xml_http.send();
-//     xmlDoc = jQuery.parseXML(xml_http.responseText);
-//     $(xmlDoc).find('row').each(function(){            
-//             var value = parseFloat($(this).children('v').text());
-//             if(!isNaN(value)){                
-//                 count += 1;
-//                 sum += value;
-//             }            
-//             });    
-//     return sum/count;
-//   }
-
-/*get specific metric usg data in last hour*/
-// function get__data()
 
 /*get specified host's update interval*/
 function get_update_host(req_url){            
--- ganglia/static/ganglia/dist/js/res_data.js
+++ ganglia/static/ganglia/dist/js/res_data.js
@@ -1,21 +1,88 @@
 var mtc_plot;
 
 $(function(){
-    mtc_plot = $.plot("#mtc-chart", get_plot_data(mtc_name[0],'1'), {
+    mtc_plot = plot_mtc("#mtc-chart",mtc_name[0],'1');    
+});
+
+
+/*
+ * basic plot func 
+ */
+function plot_mtc(elem_id,mtc_name,time_slot){
+    var time_fmt,tooltip_op;
+    if(time_slot == '1'){
+        time_fmt = "%H:%M";
+        tooltip_op = {
+            hour: "2-digit", minute: "2-digit", second: "2-digit"
+        };
+    }
+    if(time_slot == '2'){
+        time_fmt = "%y/%m/%d %H:%M";        
+        tooltip_op = {
+            month: "short",day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
+    }
+    if(time_slot == '3'){
+        time_fmt = "%y/%m/%d";        
+        tooltip_op = {
+            month: "short",day: "numeric"
+        };
+    }
+    if(time_slot == '4'){
+        time_fmt = "%y/%m/%d";        
+        tooltip_op = {
+            month: "short",day: "numeric"
+        };
+    }
+    var obj_plot = $.plot(elem_id, get_plot_data(mtc_name,time_slot), {
+        xaxis: {
+                    mode: "time",                  
+                    timeformat: time_fmt
+                },
         grid: {
                 borderColor: "#f3f3f3",
                 borderWidth: 1,
+                hoverable: true,
+                clickable: true,
                 tickColor: "#f3f3f3"
               },
-        series: {
-            stack: false,
+        series: {            
             lines: {
                 show: true,
                 fill: true
             }            
+        },
+        legend: {
+            noColumns: 4,
+            position: 'nw'
         }
-    });        
-});
+    });
+
+    $("<div id='tooltip'></div>").css({
+            position: "absolute",
+            display: "none",
+            border: "1px solid #66ffee",
+            padding: "2px",
+            "background-color": "#ffeeff", 
+            "border-radius":"3px",
+            opacity: 0.9
+        }).appendTo("body");
+
+    $(elem_id).bind("plothover", function (event, pos, item) {
+            if (item) {                
+                var x = item.datapoint[0]/1000,
+                y = item.datapoint[1].toFixed(2);
+                var t = new Date(1970,0,1);                              
+                t.setSeconds(x);
+                $("#tooltip").html(t.toLocaleDateString("en-US",tooltip_op)+"<br/>"+item.series.label+": "+y).  
+                css({top: item.pageY+5, left: item.pageX+5}).
+                fadeIn(150);
+            } else {
+                $("#tooltip").hide();
+            }
+    });
+}
+
 
 function get_plot_data(metric_name,time_slot){
   var url = "http://114.212.189.132:8000/ganglia/xml/" + hostname + '/' + metric_name + '/' + time_slot +'/';
@@ -38,7 +105,3 @@ function get_mtc_msg(req_url){
             });
     return v_array;
   }
-
-/*
- * get metric msg using req_url
- */
