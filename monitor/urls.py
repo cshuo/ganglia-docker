2a5fb829d9f6bdb19cfe8b57064406308f378340
cshuo <cshuo@cshuo.com>
2015-05-01 22:10:36 +0800

    modify url

--- monitor/urls.py
+++ monitor/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'monitor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^ganglia/', include('ganglia.urls',namespace="ganglia")),
+    url(r'^', include('ganglia.urls',namespace="ganglia")),
     url(r'^admin/', include(admin.site.urls)),
 )
